d4830a3882736eb2596d3dc33de4579db04dde5d
llubaka <llubaka1995@gmail.com>
2024-02-24 09:56:02 +0200

    Finish cart functions.

--- webapp/src/components/header/Header.tsx
+++ webapp/src/components/header/Header.tsx
@@ -1,10 +1,19 @@
 import { Image } from "../common/Image";
-
 import Settings from "../../settings/appSettings.json";
 import "./header.scss";
 import { Link } from "react-router-dom";
+import { useCartContext } from "../../context/cartContext";
+import { useCallback } from "react";
 
 export const Header: React.FC = () => {
+  const { cart } = useCartContext();
+
+  const getCartCount = useCallback(() => {
+    if (!cart.length) return 0;
+
+    return cart.reduce((acc, curr) => acc + curr.count, 0);
+  }, [cart]);
+
   return (
     <header className="header">
       <div className="header__container">
@@ -12,9 +21,10 @@ export const Header: React.FC = () => {
           <Image className="header__container__image--logo" src={Settings.images.logo} />
         </Link>
         <button>
-          <Image className="header__container__image--burger" src={Settings.images.burgerMenu} />{" "}
+          <Image className="header__container__image--burger" src={Settings.images.burgerMenu} />
         </button>
-        <button>
+        <button className="header__container__cart-button">
+          <div className="header__container__cart-button--count">{getCartCount()}</div>
           <Image className="header__container__image--cart" src={Settings.images.cart} />
         </button>
       </div>
