cdd82857472e8ef3cf4594701abc49ddc3f24a94
llubaka <llubaka1995@gmail.com>
2024-08-01 16:16:53 +0300

    X

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,17 +1,17 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { ContentType, NavBanner } from "../../components/navBanner/NavBanner";
-import { getById, getByIds } from "../../data/getData/getFilteredProducts";
+import { getById } from "../../data/getData/getFilteredProducts";
 import {
   getLocalStorageItem,
   LocalStorageKeys,
 } from "../../helpers/localStorageFunctions";
 import "./cart.scss";
 import { ProductType } from "../../globals/ProductType";
+import { getImageUrl } from "../../data/getData/getImageUrl";
 type CartProductType = { count: number; product: ProductType }[];
 
 export const Cart = () => {
-  const [products, setProducts] = useState<CartProductType>();
-  const x = (): CartProductType => {
+  const getCartProducts = useCallback((): CartProductType => {
     const lsCart = getLocalStorageItem(LocalStorageKeys.CART) as [] | null;
     if (!lsCart) throw new Error("");
 
@@ -21,12 +21,37 @@ export const Cart = () => {
         count: el.count as number,
       };
     });
-  };
-  console.log(x());
+  }, []);
 
+  const [products, setProducts] = useState<CartProductType>(getCartProducts());
   return (
     <div>
       <NavBanner contentType={ContentType.INFO} content="Количка" />
+      <div className="cart-content">
+        {products.map(({ product, count }) => {
+          const source = getImageUrl(product.imageUrl);
+          const altAtr = product.imageUrl.split(".")[0];
+          const discountText = `-${product.discount}%`;
+          return (
+            <div>
+              <div className="cart-content__item">
+                {!!product.discount && (
+                  <div className="cart-content__item--image__container--discount">
+                    {discountText}
+                  </div>
+                )}
+                <img
+                  className="cart-content__item--image__container--image"
+                  src={source}
+                  alt={altAtr}
+                />
+
+                <div>{product.info}</div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
