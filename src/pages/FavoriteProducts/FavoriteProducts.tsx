04be9e981e49b8f407ccbb1d727d92b429b7bfc4
llubaka <llubaka1995@gmail.com>
2024-08-03 10:43:16 +0300

    X

--- src/pages/FavoriteProducts/FavoriteProducts.tsx
+++ src/pages/FavoriteProducts/FavoriteProducts.tsx
@@ -1,15 +1,33 @@
+import { useNavigate } from "react-router-dom";
 import { DisplayProducts } from "../../components/displayProducts/DisplayProducts";
 import { ContentType, NavBanner } from "../../components/navBanner/NavBanner";
 import { getFavorites } from "../../data/getData/getFilteredProducts";
+import { ProductType } from "../../globals/ProductType";
 import "./favoriteProducts.scss";
+import { getHomeRouteLink } from "../../globals/Routes";
 
 export const FavoriteProducts = () => {
-  const favoriteProducts = getFavorites();
+  const navigate = useNavigate();
+
+  let favoriteProducts: ProductType[] = [];
+
+  const navigateToHome = () => {
+    setTimeout(() => {
+      navigate(getHomeRouteLink());
+    }, 0);
+  };
+
+  try {
+    favoriteProducts = getFavorites();
+    if (favoriteProducts.length === 0) navigateToHome();
+  } catch (e) {
+    navigateToHome();
+  }
 
   return (
     <div>
       <NavBanner contentType={ContentType.INFO} content="Любими" />
-      <DisplayProducts products={favoriteProducts} />
+      {favoriteProducts && <DisplayProducts products={favoriteProducts} />}
     </div>
   );
 };
