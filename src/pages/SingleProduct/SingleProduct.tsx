ed87febf4aaf92ff0dc7f721298f8c98cdf6e677
llubaka <llubaka1995@gmail.com>
2024-08-13 19:11:01 +0300

    X

--- src/pages/SingleProduct/SingleProduct.tsx
+++ src/pages/SingleProduct/SingleProduct.tsx
@@ -92,50 +92,55 @@ export const SingleProduct = () => {
           Направи поръчка
         </button>
       </div>
-      <hr className="sp-hr-separator" />
+
       <div className="sp-additional-info">
-        {(contact.telephone || contact.email) && (
-          <div className="sp-additional-info__container">
-            {contact.telephone && (
-              <div className="sp-additional-info--first">
-                <a
-                  className="sp-additional-info--href"
-                  href={`tel:${contact.telephone}`}
-                >
-                  <div className="sp-additional-info--href__container">
-                    Имате въпроси или желаете да поръчате?
-                  </div>
-                  <div className="sp-additional-info--href--contact">
-                    <span className="sp-additional-info--icon">
-                      <Telephone color="#e39606" />
-                    </span>
-                    {contact.telephone}
-                  </div>
-                </a>
-              </div>
-            )}
-            {contact.email && (
-              <div>
-                <a
-                  className="sp-additional-info--href"
-                  href={`mailto:${contact.email}`}
-                >
-                  <div className="sp-additional-info--href__container">
-                    Може да се свържете с нас и по имейл:
-                  </div>
-                  <div className="sp-additional-info--href--contact">
-                    <span className="sp-additional-info--icon">
-                      <Envelope color="#e39606" />
-                    </span>
-                    {contact.email}
-                  </div>
-                </a>
-              </div>
-            )}
-          </div>
-        )}
-        <hr className="sp-additional-separator" />
-        <Accordion items={product.description} />
+        <div className="sp-contact-container">
+          <div className="sp-div-separator"></div>
+          {(contact.telephone || contact.email) && (
+            <div className="sp-additional-info__container">
+              {contact.telephone && (
+                <div className="sp-additional-info--first">
+                  <a
+                    className="sp-additional-info--href"
+                    href={`tel:${contact.telephone}`}
+                  >
+                    <div className="sp-additional-info--href__container">
+                      Имате въпроси или желаете да поръчате?
+                    </div>
+                    <div className="sp-additional-info--href--contact">
+                      <span className="sp-additional-info--icon">
+                        <Telephone color="#e39606" />
+                      </span>
+                      {contact.telephone}
+                    </div>
+                  </a>
+                </div>
+              )}
+              {contact.email && (
+                <div>
+                  <a
+                    className="sp-additional-info--href"
+                    href={`mailto:${contact.email}`}
+                  >
+                    <div className="sp-additional-info--href__container">
+                      Може да се свържете с нас и по имейл:
+                    </div>
+                    <div className="sp-additional-info--href--contact">
+                      <span className="sp-additional-info--icon">
+                        <Envelope color="#e39606" />
+                      </span>
+                      {contact.email}
+                    </div>
+                  </a>
+                </div>
+              )}
+            </div>
+          )}
+          <div className="sp-div-separator"></div>{" "}
+        </div>
+        <div className="sp-accordion-container">
+          <Accordion items={product.description} />
+        </div>
       </div>
     </>
   );
