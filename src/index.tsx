a55d1a4bb585f6df34091ae6ba0f17a47d8a225b
llubaka <llubaka1995@gmail.com>
2024-08-15 18:42:44 +0300

    X

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./index.scss";
 import App from "./app/App";
 import { ErrorBoundary } from "react-error-boundary";
 import { BrowserRouter } from "react-router-dom";
